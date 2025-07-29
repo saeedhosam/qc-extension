ac4cf46d7e74b5ed27a4290760a077f50bcc7947
Speedy <makymaky757@gmail.com>
2025-07-29 13:38:02 +0000

    oiwjfeow

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,5 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-    <div>Hello World</div>
+    <div className='text-3xl font-bold underline'>Hello World</div>
 )
