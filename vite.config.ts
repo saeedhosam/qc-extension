154af62f8e4ac04a242cf5752927d2427e9ce482
Speedy <makymaky757@gmail.com>
2025-07-29 13:20:55 +0000

    jowfew

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-    outDir: 'build',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
-
 })
