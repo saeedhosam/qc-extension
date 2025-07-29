0de4bd32f9b2c6e2394eb2c71f03130b8814ca8a
Speedy <makymaky757@gmail.com>
2025-07-29 12:59:36 +0000

    jowefjiew

--- old/background.js
+++
@@ -1,4 +0,0 @@
-// This function opens the side panel when the extension icon is clicked.
-chrome.action.onClicked.addListener((tab) => {
-  chrome.sidePanel.open({ windowId: tab.windowId });
-});
--- src/background.ts
+++
@@ -1,6 +0,0 @@
-// This function opens the side panel when the extension icon is clicked.
-chrome.action.onClicked.addListener((tab) => {
-  if (tab.windowId) {
-    chrome.sidePanel.open({ windowId: tab.windowId });
-  }
-});
