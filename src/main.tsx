44d8e5550b120ce41e022b8a0b986fce3841b2b0
saeedhosam <makymaky757@gmail.com>
2025-07-30 11:53:00 +0300

    jwioejfwe

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import "@material/web/checkbox/checkbox.js";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-    <div className='text-3xl font-bold underline'>Hello World</div>
-)
+createRoot(document.getElementById("root")!).render(
+  <div className="border">
+    <md-checkbox label="Accept Terms"></md-checkbox>
+  </div>,
+);
