7e0f92e5e8606f57893aac9bfc29e6ca812d1fe2
Speedy <makymaky757@gmail.com>
2025-07-30 13:24:11 +0000

    owjefowji

--- dist/background.js
+++ dist/background.js
@@ -1,9 +1,13 @@
-chrome.action.onClicked.addListener(async (tab) => {
-  const { windowId } = tab;
-  const info = await chrome.sidePanel.getPanelBehavior({ windowId });
-  if (info.opened) {
-    chrome.sidePanel.close({ windowId });
+const panelState = {};
+
+chrome.action.onClicked.addListener((tab) => {
+  const winId = tab.windowId;
+
+  if (panelState[winId]) {
+    chrome.sidePanel.close({ windowId: winId });
+    panelState[winId] = false;
   } else {
-    chrome.sidePanel.open({ windowId });
+    chrome.sidePanel.open({ windowId: winId });
+    panelState[winId] = true;
   }
 });
