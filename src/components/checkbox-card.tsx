a9fa12e8004114adaaafbf287492305dd80e92c4
saeedhosam <makymaky757@gmail.com>
2025-07-31 04:11:59 +0300

    alhamdulillah

--- src/components/checkbox-card.tsx
+++ src/components/checkbox-card.tsx
@@ -3,10 +3,10 @@ import { useReducer } from "react"
 export const Checkbox = ({ text }) => {
   return (
     <label
-      className="flex w-full items-center m-4 px-4 py-2 text-sm rounded-lg transition-all border-none cursor-pointer
+      className="flex w-full items-center my-2 px-4 py-2 text-sm rounded-lg transition-all border-none cursor-pointer
       shadow-lg hover:shadow-md active:scale-105 bg-slate-50 hover:bg-slate-100 text-slate-800 hover:text-slate-900">
       <input type="checkbox" className="mr-2 cursor-pointer" />
-      <span className="inline-flex items-center select-none justify-center h-4 ml-2 text-xs font-semibold rounded-full">
+      <span className="inline-flex items-center select-none justify-center ml-2 text-xs rounded-full">
         {text}
       </span>
     </label>
--- src/sidepanel.tsx
+++ src/sidepanel.tsx
@@ -1,12 +1,56 @@
+import { useEffect, useRef, useState } from "react"
+
 import { Checkbox } from "~components/checkbox-card"
 
 import "~style.css"
 
 function IndexPopup() {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const menuRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleOutsideClick)
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick)
+    }
+  }, [isOpen])
+
   return (
-    <div className="flex justify-center">
-      <Checkbox text="hello world"/>
-      <Checkbox text="welcome"/>
+    <div className="flex flex-col items-center justify-center mt-5">
+      <div className="relative flex flex-col justify-center" ref={menuRef}>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white mb-2 font-bold py-2 px-4 rounded cursor-pointer select-none focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}>
+         Category
+        </button>
+        <ul
+          className={`bg-white rounded w-40 border transition-all mb-5 duration-300 overflow-hidden ${
+            isOpen ? "max-h-40 p-2" : "max-h-0 border-0 p-0"
+          }`}>
+          <li className="p-2 text-center hover:bg-gray-100 cursor-pointer select-none">
+            Issuance
+          </li>
+          <li className="p-2 text-center hover:bg-gray-100 cursor-pointer select-none">
+            Exchange
+          </li>
+          <li className="p-2 text-center hover:bg-gray-100 cursor-pointer select-none">
+            Refund
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-col items-center px-4 w-full">
+        <div className="w-full max-w-md">
+          <Checkbox text="Rubik woiejfpowiej fpoiwjepfo iwjepfoijw epofijwpeoifj pwoeijf " />
+          <Checkbox text="test 2" />
+          <Checkbox text="test 3" />
+        </div>
+      </div>
     </div>
   )
 }
