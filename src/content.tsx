54f83d9aa0214fd6f514ce516310ab62f303b8ec
saeedhosam <makymaky757@gmail.com>
2025-07-31 12:42:07 +0300

    fyujkdfty

--- src/content.tsx
+++ src/content.tsx
@@ -1,8 +1,6 @@
 import cssText from "data-text:~style.css"
 import type { PlasmoCSConfig } from "plasmo"
 
-import { CountButton } from "~components/checkbox-card"
-
 export const config: PlasmoCSConfig = {
   matches: ["<all_urls>"]
 }
@@ -40,7 +38,7 @@ export const getStyle = (): HTMLStyleElement => {
 const PlasmoOverlay = () => {
   return (
     <div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8">
-      <CountButton />
+            hi
     </div>
   )
 }
